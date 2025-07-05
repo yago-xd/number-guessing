0772678131949f5b0a53daa04449945675a94b83
yago-xd <srijanbhadra41@gmail.com>
2025-07-05 16:01:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# 🎯 Number Guessing Game
+
+A beginner made Java game where the user must guess a randomly generated number from 1 to 100.
+
+This is my first java project
+
+![Java](https://img.shields.io/badge/Language-Java-blue.svg)
+![Beginner Project](https://img.shields.io/badge/Level-Beginner-brightgreen)
+![Console Game](https://img.shields.io/badge/Type-Console--App-lightgrey)
+
+---
+
+## 📸 Demo of the program
+```console
+Welcome to the number guessing game
+
+Guess a number between 1 to 100: 50
+Too low. Guess again!
+Guess a number between 1 to 100: 75
+Too high. Guess again!
+Guess a number between 1 to 100: 63
+Congrats! You have guessed the number 63 correctly.
+It took you 3 attempts to guess it
+
+Do you want to play again? (Y/N):
+```
+
+---
+
+## 🧠 Features
+
+✅ Random number between 1–100  
+✅ Validates integer input  
+✅ Counts attempts taken by the user
+✅ Replay feature (`Y/N`)  
+✅ Clean and readable code  
+✅ Commented and structured properly
+
+---
+
+## 🚀 How to Run
+
+1. Clone this repository or download the `.zip`
+2. Open the project in **IntelliJ IDEA** or any Java IDE
+3. Run `Main.java` file
+4. Follow the on-screen instructions
+
+> Requires **Java 8 or above**
+
+---
+
+## 🔧 Tools Used
+
+- Java 17 (or Java 8+)
+- IntelliJ IDEA (recommended)
+- Git + GitHub
+
+---
+
+## ✏️ Author
+
+Made with ❤️ by Yago  
+📫 [GitHub Profile](https://github.com/yago-xd)
+
+---
+
+## 🌟 Future Improvements
+
+- Add timer to track guess time
+- Add difficulty modes
+- Add GUI using Swing
+- Track the best score per session
+- Allow user to input a range
+- Convert program to OOP based logic
+
+---
+
+> 💡 This project was built as part of learning Object-Oriented Programming and core Java concepts.
---
+++ src/Main.java
@@ -0,0 +1,61 @@
+// A simple java program to guess a number between 1 and 100
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Main method to start and run the game with replay support.
+        Scanner sc = new Scanner(System.in);// creating a scanner object to read user input
+        Random rand = new Random();// to generate the random number
+        System.out.println("Welcome to the number guessing game");
+        //initializing variables for use
+        int guess;
+        int tries=0;//initializing a variable to keep track of a number of tries before the user guesses it
+        int num = rand.nextInt(100) + 1;//generating random number from 1-100(inclusive)
+        char playagain ='Y';//setting playagain to Y to allow the first round of game to occur
+        boolean guessed=false;//initially setting guessed to false as a round has not been completed yet
+        while (playagain =='Y'|| playagain =='y') {
+            // core loop to allow replay of game till user wishes to play by entering Y when prompted to
+            if(guessed){
+                //if the previous round was complete, reset the state and generate new number
+                num = rand.nextInt(100) + 1;
+                tries=0;
+                guessed=false;
+            }
+            System.out.print("\nGuess a number between 1 to 100: ");
+            if(sc.hasNextInt())//checking to make sure if entered input is of int value
+                guess = sc.nextInt();//read input and store it if it is a valid value
+            else{
+                System.out.println("Invalid input, must be an integer");
+                sc.next();//clear invalid input
+                continue;//skip this iteration
+            }
+            if (guess > 100 || guess < 1) {//check to make sure the value is in between 1 and 100
+                System.out.println("Invalid input, your guess must be within 1 to 100");
+            }
+            else {//core logic
+                if (guess > num) {//too high guess block
+                    System.out.println("Too high. Guess again!");
+                    tries++;
+                }
+                else if (guess < num) {//too low guess block
+                    System.out.println("Too low. Guess again!");
+                    tries++;
+                }
+                else{//correct guess block
+                    System.out.println("Congrats! You have guessed the number " + num + " correctly.");
+                    tries++;
+                    if (tries == 1)
+                        System.out.println("It took you only 1 attempt to guess it");
+                    else
+                        System.out.println("It took you " + tries + " attempts to guess it");
+                    System.out.print("\nDo you want to play again? (Y/N): ");//if a user wants to replay
+                    playagain =sc.next().trim().charAt(0);//storing new choice (y/n)
+                    guessed=true;//setting guessed as true as a round has been completed
+                    //this allows the game to reset the state and generate a new number for the next round
+                }
+            }
+        }
+        System.out.println("Thanks for playing!");
+        sc.close();
+    }
+}
